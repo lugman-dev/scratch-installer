4c57479e6a5452916d4080f103b4b010e9c320e9
Michele Giacomoli <michele.giacomoli@gmail.com>
2019-06-25 19:34:51 +0200

    Added AUTHORS file

--- Scratchinstall.py
+++ Scratchinstall.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
-#  Luglio 2018 - Stefano Salvi stefano@salvi.mn.it
-#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
